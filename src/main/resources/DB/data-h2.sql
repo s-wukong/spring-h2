24b44559a591b6d41c956b5c1117366aee15dcd3
sunzb <sunzb@cig.com.cn>
2020-12-10 10:39:34 +0800

    fix(#2) h2嵌入式数据库

--- src/main/resources/DB/data-h2.sql
+++ src/main/resources/DB/data-h2.sql
@@ -2,5 +2,5 @@ INSERT INTO EMPLOYEE (id, name) VALUES (1, 'DHONI');
 INSERT INTO EMPLOYEE (id, name) VALUES (2, 'KHOLI');
 
 
-INSERT INTO USER (id,name ) value (1,'老五');
-INSERT INTO USER (id,name ) value (2,'老六');
+-- INSERT INTO USER (id,name ) value (1,'老五');
+-- INSERT INTO USER (id,name ) value (2,'老六');
--- src/main/resources/DB/schema-h2.sql
+++ src/main/resources/DB/schema-h2.sql
@@ -5,9 +5,9 @@ CREATE TABLE EMPLOYEE
         name VARCHAR2(50) NOT NULL,
         PRIMARY KEY(id)
         );
-CREATE  TABLE  USER
-(
-id NUMBER(10) NOT NULL,
-        name VARCHAR2(50) NOT NULL,
-        PRIMARY KEY(id)
-);
+-- CREATE  TABLE  USER
+-- (
+-- id NUMBER(10) NOT NULL,
+--         name VARCHAR2(50) NOT NULL,
+--         PRIMARY KEY(id)
+-- );
